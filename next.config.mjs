6767719b8d9e364cc274212526fea79bb5a5788b
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-09-30 19:56:35 +0500

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
-    // basePath: '/test-task-user-record',
-    // assetPrefix: '/test-task-user-record/',
 };
 
 export default nextConfig;
