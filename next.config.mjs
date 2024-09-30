0ae52e9bc093ae44452b8f08ea34ff9bf1dc54d2
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-09-30 19:25:33 +0500

    extend config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/test-task-user-record',
+    assetPrefix: '/test-task-user-record/',
+};
 
 export default nextConfig;
