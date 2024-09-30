6c26441c080a7ae52c1bcc264b51989554665b66
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-09-30 19:14:18 +0500

    Updates

--- pages/profile.js
+++
@@ -1,7 +0,0 @@
-import Profile from "@/components/profile";
-
-function ProfilePage() {
-  return <Profile />;
-}
-
-export default ProfilePage;
--- pages/register.js
+++
@@ -1,7 +0,0 @@
-import RegistrationForm from "@/components/RegistrationForm";
-
-function RegisterPage() {
-  return <RegistrationForm />;
-}
-
-export default RegisterPage;
